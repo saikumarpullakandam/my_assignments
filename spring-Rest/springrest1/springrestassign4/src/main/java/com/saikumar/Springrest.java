daa9bd783b164f1da94372354f4dfe21d61a658e
saikumarpullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2021-11-28 15:21:33 +0530

    Add files via upload

---
+++ spring-Rest/springrest1/springrestassign4/src/main/java/com/saikumar/Springrest.java
@@ -0,0 +1,20 @@
+package com.saikumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Springrest {
+
+	
+		public static void main (String[]args) {
+			SpringApplication.run(Springrest.class,args);
+			
+		}
+		
+	
+
+	}
+
---
+++ spring-Rest/springrest1/springrestassign4/src/test/java/springrest4/springrestassign4/Springrestassign4ApplicationTests.java
@@ -0,0 +1,13 @@
+package springrest4.springrestassign4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrestassign4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
