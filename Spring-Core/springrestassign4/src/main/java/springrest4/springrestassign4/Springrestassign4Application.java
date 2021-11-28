51db530543cc94405e55274b007b1ef6fc8daf89
saikumarpullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2021-11-28 15:11:21 +0530

    Add files via upload

---
+++ Spring-Core/springrestassign4/src/main/java/springrest4/springrestassign4/Springrestassign4Application.java
@@ -0,0 +1,13 @@
+package springrest4.springrestassign4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrestassign4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrestassign4Application.class, args);
+	}
+
+}
