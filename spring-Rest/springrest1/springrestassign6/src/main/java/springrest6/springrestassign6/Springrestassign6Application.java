7b98f2914af6d509bf3b30b414ce9c4b80e6e142
saikumarpullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2021-11-28 15:22:34 +0530

    Add files via upload

---
+++ spring-Rest/springrest1/springrestassign6/src/main/java/springrest6/springrestassign6/Springrestassign6Application.java
@@ -0,0 +1,13 @@
+package springrest6.springrestassign6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrestassign6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrestassign6Application.class, args);
+	}
+
+}
---
+++ spring-Rest/springrest1/springrestassign6/src/test/java/springrest6/springrestassign6/Springrestassign6ApplicationTests.java
@@ -0,0 +1,13 @@
+package springrest6.springrestassign6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrestassign6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
